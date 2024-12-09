c74e4c7d5f7a805cc2871da1b1bc0bc00f8d11d3
Sajan Kumar Kar <sajan.kumar.kar@gmail.com>
2024-12-09 13:32:08 -0500

    Possible model options added for inference code

--- Code/finetune/Inference_sajan.py
+++ Code/finetune/Inference_sajan.py
@@ -1,15 +1,24 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import os
 
 # Check for GPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
+os.chdir('../')
 
+# Use the below 3 lines for base model
 # model_name = "meta-llama/Llama-3.2-1B"
 # model = AutoModelForCausalLM.from_pretrained(model_name)
 # tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained("./fine_tuned_modelpf_lora_epoch_5/")
-tokenizer = AutoTokenizer.from_pretrained("./fine_tuned_modelpf_lora_epoch_5/")
+
+# Use the below 2 lines if you trained your model and want to use it
+# model = AutoModelForCausalLM.from_pretrained("./fine_tuned_modelpf_lora_epoch_5/")
+# tokenizer = AutoTokenizer.from_pretrained("./fine_tuned_modelpf_lora_epoch_5/")
+
+# Use the below lines for loading out model for inferencing
+model = AutoModelForCausalLM.from_pretrained("./models/")
+tokenizer = AutoTokenizer.from_pretrained("./models/")
 model.to(device)
 
 def doctor_response(question, max_length=300, temperature=0.6):
@@ -62,7 +71,7 @@ if __name__ == "__main__":
     # user_input = input("You (Patient): ")
     # user_input = "I randomly get nauseatic and I have been constantly vomiting for the last few days. I also have a mild fever. What do you think is wrong with me?"
     # user_input = "Hi doctor,I am just wondering what is abutting and abutment of the nerve root means in a back issue. Please explain. What treatment is required for annular bulging and tear?"
-    user_input = "Hi doctor, what food should I eat in my third trimester?"
-    # user_input = "I have a sharp pain in my chest that radiates to my left arm. I feel dizzy and short of breath. What should I do?"
+    # user_input = "Hi doctor, what food should I eat in my third trimester?"
+    user_input = "I have a sharp pain in my chest that radiates to my left arm. I feel dizzy and short of breath. What should I do?"
     answer = doctor_response(user_input)
     print(answer)
